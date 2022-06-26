0a53053e6b11fc3c8d64d118ca6c088b6b41ebf0
Vasily Muraviev <muravievvv@develop.mos.ru>
2022-06-26 13:40:02 +0300

    Add open popup event to Main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,19 @@
 function Main() {
+    function handleEditAvatarClick() {
+        const popup = document.querySelector('.popup_avatar-change');
+        popup.classList.add('popup_opened');
+    }
+
+    function handleEditProfileClick() {
+        const popup = document.querySelector('.popup_profile');
+        popup.classList.add('popup_opened');
+    }
+
+    function handleAddPlaceClick() {
+        const popup = document.querySelector('.popup_place');
+        popup.classList.add('popup_opened');
+    }
+
     return (
         <>
             <main>
@@ -6,14 +21,14 @@ function Main() {
                 <section className="profile">
                     <div className="profile__avatar">
                         <img alt="аватар" className="profile__avatar-img"/>
-                        <button type="button" className="profile__avatar-button"></button>
+                        <button type="button" className="profile__avatar-button" onClick={handleEditAvatarClick}></button>
                     </div>
                     <div className="info">
                         <h1 className="info__title"></h1>
-                        <button type="button" className="info__redact-button"></button>
+                        <button type="button" className="info__redact-button" onClick={handleEditProfileClick}></button>
                         <p className="info__subtitle"></p>
                     </div>
-                    <button type="button" className="profile__add-place-button"></button>
+                    <button type="button" className="profile__add-place-button" onClick={handleAddPlaceClick}></button>
                 </section>
 
                 <section className="places">
