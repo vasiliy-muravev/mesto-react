27e3719dfb8e3e0d84b144e030ff83d67591c7ab
Vasily Muraviev <muravievvv@develop.mos.ru>
2022-07-10 17:57:00 +0300

    Like fixe

--- src/components/Card.js
+++ src/components/Card.js
@@ -15,7 +15,7 @@ function Card({card, onCardClick, onDeleteClick}) {
 
     /* Определяем, есть ли у карточки лайк, поставленный текущим пользователем */
     const isLiked = card.likes.some(i => i._id === user._id);
-    const cardLikeButtonClassName = (`description__like-count ${isLiked ? 'description__like_active' : ''}`);
+    const cardLikeButtonClassName = (`description__like ${isLiked ? 'description__like_active' : ''}`);
 
     return (
         <article className="place" id={card._id}>
@@ -28,8 +28,8 @@ function Card({card, onCardClick, onDeleteClick}) {
             <div className="description">
                 <h2 className="description__title">{card.name}</h2>
                 <div className="description__like-container">
-                    <button type="button" className="description__like"/>
-                    <p className={cardLikeButtonClassName}>{card.likes ? card.likes.length : 0}</p>
+                    <button type="button" className={cardLikeButtonClassName}/>
+                    <p className="description__like-count">{card.likes ? card.likes.length : 0}</p>
                 </div>
             </div>
         </article>
